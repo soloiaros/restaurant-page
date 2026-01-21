f7937362885ddd835afdc012078b0c4edfa89c93
soloiaros <soloiaros@gmail.com>
2026-01-21 09:00:53 +0300

    Update the default scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { watchFile } = require('fs');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
